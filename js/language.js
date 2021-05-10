5afb739aa33ca1580f6a1f4c4c4d7ff58683c8a8
Shruti3004 <mail2shruti.ag@gmail.com>
2021-05-10 15:25:06 +0530

    fixed the suggested changes

---
+++ js/language.js
@@ -0,0 +1,31 @@
+var searchBoard = document.querySelector("#firstSearch");
+var searchLang = document.querySelector("#secondSearch");
+var list1 = document.querySelector(".first");
+var list2 = document.querySelector(".second");
+
+searchBoard.addEventListener("keyup", Search1);
+searchLang.addEventListener("keyup", Search2);
+
+function Search1() {
+  var list = list1.getElementsByTagName("label");
+  var val = searchBoard.value.toLowerCase();
+  Array.from(list).forEach((listItem) => {
+    var text = listItem.textContent;
+    if (text.toLowerCase().indexOf(val) != -1) {
+      listItem.parentElement.style.display = "block";
+    } else {
+      listItem.parentElement.style.display = "none";
+    }
+  });
+}
+
+function Search2() {
+  var list = list2.getElementsByTagName("label");
+  var val = searchLang.value.toLowerCase();
+  Array.from(list).forEach((listItem) => {
+    var text = listItem.textContent;
+    if (text.toLowerCase().indexOf(val) != -1) {
+      listItem.parentElement.style.display = "block";
+    } else listItem.parentElement.style.display = "none";
+  });
+}
