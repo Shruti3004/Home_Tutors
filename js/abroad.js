0df7418110786e136ac4cb05adeaaf4b1836fb01
paarth17032000 <paarth17032000@gmail.com>
2021-05-15 23:12:05 +0530

    navbar modified

---
+++ js/abroad.js
@@ -0,0 +1,26 @@
+var searchBoard = document.querySelector("#firstSearch");
+
+var list1 = document.querySelectorAll(".accordion-button");
+// console.log(list2)
+// console.log(Array.from(list1.innerText))
+
+searchBoard.addEventListener("keyup", Search1);
+
+function Search1() {
+//     list1 = Array.from(list1);
+//   list2 = Array.from(list2.getElementsByTagName("label"));
+//   var list = list1;
+//   console.log(list2);
+//   var main = [...list1,...list2]
+//   console.log(main)
+  var val = searchBoard.value.toLowerCase();
+//   Array.from(list1)
+  Array.from(list1).forEach((listItem) => {
+    var text = listItem.textContent;
+    if (text.toLowerCase().indexOf(val) != -1) {
+      listItem.parentElement.parentElement.style.display = "block";
+    } else {
+      listItem.parentElement.parentElement.style.display = "none";
+    }
+  });
+}
